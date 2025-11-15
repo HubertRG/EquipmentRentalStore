d5ee5b2f57632cb6471a24a48da10e27602befc0
HubertRG <hubertgosik1@gmail.com>
2025-11-15 18:35:55 +0100

    feat(backend): added authorization controller
    - Added signup and login endpoints for the user

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ const path = require("path");
 const app = express();
 
 //Importy routerów kontrolerów
+const authorizationRoutes = require("./src/controllers/authController.js");
 const userRoutes = require("./src/controllers/userController.js");
 
 app.use(express.json());
 
 //Przypisanie tras
+app.use("/authorization", authorizationRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/user", userRoutes);
 
