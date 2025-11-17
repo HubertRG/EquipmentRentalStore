dc531064feb6fdfbc203febbac9250acc3cc734f
HubertRG <hubertgosik1@gmail.com>
2025-11-17 20:17:57 +0100

    feat(frontend): added register form component
    - Added register form with validation and success notification
    - Changed components folder structure
    - Updated imports in other views

--- frontend/src/views/ErrorPage.jsx
+++ frontend/src/views/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router";
-import Footer from "../components/Footer";
+import Footer from "../components/Universal/Footer";
+import Navbar from "../components/Universal/Navbar";
 
 /*
     Strona błędu
@@ -11,6 +12,7 @@ import Footer from "../components/Footer";
 export default function ErrorPage() {
   return (
     <div className="bg-slate-100 min-h-screen text-black dark:text-white">
+      <Navbar />
       <h2>404 - Strona nie istnieje</h2>
       <NavLink to="/">
         <h4>Powrót do strony głównej</h4>
--- frontend/src/views/Register.jsx
+++ frontend/src/views/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/Universal/Navbar";
+import Footer from "../components/Universal/Footer";
+import RegisterForm from "../components/Register/RegisterForm";
 
 /*
     Strona rejestracji
@@ -11,7 +12,10 @@ export default function Register() {
   return (
     <div className="min-h-screen bg-slate-100 dark:bg-[#242424]  transition-colors duration-300">
       <Navbar />
-      <Footer/>
+      <div className="container mx-auto px-4 py-8 flex justify-center">
+        <RegisterForm />
+      </div>
+      <Footer />
     </div>
   );
 }
